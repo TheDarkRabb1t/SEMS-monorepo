a08414f1f08959429a4978057e9e5a4a4c4aa3b6
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-06-16 19:58:14 +0300

    fixed controller mapping

--- ingestion-service/src/main/java/tdr/pet/ingestion/controller/LogRestController.java
+++ ingestion-service/src/main/java/tdr/pet/ingestion/controller/LogRestController.java
@@ -1,17 +1,21 @@
 package tdr.pet.ingestion.controller;
 
-import lombok.AllArgsConstructor;
 import model.LogEvent;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tdr.pet.ingestion.service.LogPublisherService;
 
-@AllArgsConstructor
-@RestController("/api/logs")
+@RestController
+@RequestMapping("api/logs")
 public class LogRestController {
     private final LogPublisherService logPublisherService;
+
+    public LogRestController(LogPublisherService logPublisherService) {
+        this.logPublisherService = logPublisherService;
+    }
 
     @PostMapping()
     public ResponseEntity<Void> uploadLogs(@RequestBody LogEvent logEvent) {
