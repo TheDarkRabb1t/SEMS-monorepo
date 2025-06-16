b18e77e588c8c363782950eac57a0b936d2eb911
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-06-16 17:06:20 +0300

    changed groovy to kts

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    java
+}
+
+group = "tdr.pet"
+version = "1.0.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
