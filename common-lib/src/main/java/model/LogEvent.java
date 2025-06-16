817c8c7ffd18ab299d828a2286fc3a6fd95ebb23
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-06-16 19:58:23 +0300

    made entity serializable

--- common-lib/src/main/java/model/LogEvent.java
+++ common-lib/src/main/java/model/LogEvent.java
@@ -1,6 +1,7 @@
 package model;
 
+import java.io.Serializable;
 import java.time.Instant;
 
-public record LogEvent(String message, Instant timestamp, String user, String ipAddress) {
+public record LogEvent(String message, Instant timestamp, String user, String ipAddress) implements Serializable {
 }
