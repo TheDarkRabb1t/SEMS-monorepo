797e494f88bf4bf543a8d051189e4a9a49017600
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-06-19 01:14:39 +0300

    added lombok as subprojects deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ subprojects {
 
     dependencies {
         implementation("org.slf4j:slf4j-api:2.0.17")
+        compileOnly("org.projectlombok:lombok:1.18.30")
+        annotationProcessor("org.projectlombok:lombok:1.18.30")
     }
 
     tasks.test {
