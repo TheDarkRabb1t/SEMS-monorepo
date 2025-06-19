0ae30f9bcf2b9b138a7be4ba11955f756d3eab10
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-06-20 01:43:29 +0300

    added junit for subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ subprojects {
         implementation("org.slf4j:slf4j-api:2.0.17")
         compileOnly("org.projectlombok:lombok:1.18.30")
         annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+        testImplementation(platform("org.junit:junit-bom:5.13.1"))
+        testImplementation("org.junit.jupiter:junit-jupiter-api")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
 
     tasks.test {
