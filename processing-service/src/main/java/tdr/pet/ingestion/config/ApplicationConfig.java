3e950374f6b3c876a0dc587baf94763932cedbc5
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-06-25 16:26:34 +0300

    made city database file downloadable on init

--- processing-service/src/main/java/tdr/pet/ingestion/config/ApplicationConfig.java
+++ processing-service/src/main/java/tdr/pet/ingestion/config/ApplicationConfig.java
@@ -4,17 +4,17 @@ import com.maxmind.geoip2.DatabaseReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import ua_parser.Parser;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public DatabaseReader databaseReader(@Value("${geoip.dbPath}") Resource dbResource) throws IOException {
-        return new DatabaseReader.Builder(dbResource.getInputStream()).build();
+    public DatabaseReader databaseReader(@Value("${geoip.dbPath}") String dbPath) throws IOException {
+        return new DatabaseReader.Builder(Paths.get(dbPath).toFile()).build();
     }
 
     @Bean
