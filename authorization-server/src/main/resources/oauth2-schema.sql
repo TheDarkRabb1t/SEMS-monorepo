ff4b6acc83e08ccccc53d8b5c99dacca4fa7af17
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-07-01 15:16:40 +0300

    removed default admin user on init

--- authorization-server/src/main/resources/oauth2-schema.sql
+++ authorization-server/src/main/resources/oauth2-schema.sql
@@ -61,33 +61,3 @@ CREATE TABLE IF NOT EXISTS oauth2_registered_client
 
 CREATE INDEX IF NOT EXISTS idx_oauth2_client_client_id ON oauth2_registered_client (client_id);
 CREATE INDEX IF NOT EXISTS idx_oauth2_client_client_name ON oauth2_registered_client (client_name);
-
-INSERT INTO oauth2_registered_client (id,
-                                      client_id,
-                                      client_id_issued_at,
-                                      client_secret,
-                                      client_secret_expires_at,
-                                      client_name,
-                                      client_authentication_methods,
-                                      authorization_grant_types,
-                                      redirect_uris,
-                                      post_logout_redirect_uris,
-                                      scopes,
-                                      client_settings,
-                                      token_settings)
-SELECT 'admin-client-id',
-       'admin-client',
-       NOW(),
-       'admin-secret',
-       NULL,
-       'Default Admin Client',
-       'client_secret_basic',
-       'authorization_code,refresh_token',
-       'http://localhost:8080/login/oauth2/code/admin-client',
-       'http://localhost:8080/',
-       'openid,profile,email',
-       '{"requireProofKey":false,"requireAuthorizationConsent":true}',
-       '{"accessTokenTimeToLive":"PT1H","reuseRefreshTokens":true}'
-WHERE NOT EXISTS (SELECT 1
-                  FROM oauth2_registered_client
-                  WHERE client_id = 'admin-client');
