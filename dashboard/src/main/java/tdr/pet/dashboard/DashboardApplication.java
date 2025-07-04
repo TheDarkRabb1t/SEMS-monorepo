06714611c593ace3a695a3fcd66c359c9f5800c4
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-07-04 18:50:52 +0300

    fixed component scan

--- dashboard/src/main/java/tdr/pet/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/tdr/pet/dashboard/DashboardApplication.java
@@ -2,8 +2,13 @@ package tdr.pet.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "tdr.pet.dashboard",
+        "model.mapper"
+})
 public class DashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(DashboardApplication.class, args);
