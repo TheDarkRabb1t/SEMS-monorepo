233643f9dfb94c332e0f1426e1f9a8fff1693f77
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-07-07 14:31:17 +0300

    fixed RequestBody

--- dashboard/src/main/java/tdr/pet/dashboard/controller/EnrichedLogRestController.java
+++ dashboard/src/main/java/tdr/pet/dashboard/controller/EnrichedLogRestController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tdr.pet.dashboard.service.EnrichedLogService;
@@ -18,7 +19,7 @@ public class EnrichedLogRestController {
     private final EnrichedLogService enrichedLogService;
 
     @PostMapping("/search")
-    public ResponseEntity<Page<EnrichedLogEventDto>> searchEnrichedLogs(Pageable pageable, EnrichedLogEventDto enrichedLogEventDto) {
+    public ResponseEntity<Page<EnrichedLogEventDto>> searchEnrichedLogs(Pageable pageable, @RequestBody EnrichedLogEventDto enrichedLogEventDto) {
         return ResponseEntity.ok(enrichedLogService.searchByDto(enrichedLogEventDto, pageable));
     }
 }
