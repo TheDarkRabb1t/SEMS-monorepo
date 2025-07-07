6009afc85fa7258da304b21eff781dcc721566ae
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2025-07-07 13:02:35 +0300

    fixed url mapping

--- dashboard/src/main/java/tdr/pet/dashboard/controller/EnrichedLogRestController.java
+++ dashboard/src/main/java/tdr/pet/dashboard/controller/EnrichedLogRestController.java
@@ -6,16 +6,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tdr.pet.dashboard.service.EnrichedLogService;
 
+@RestController
 @AllArgsConstructor
-@RestController("/api/enriched-logs")
+@RequestMapping("/api/enriched-logs")
 public class EnrichedLogRestController {
 
     private final EnrichedLogService enrichedLogService;
 
-    @PostMapping
+    @PostMapping("/search")
     public ResponseEntity<Page<EnrichedLogEventDto>> searchEnrichedLogs(Pageable pageable, EnrichedLogEventDto enrichedLogEventDto) {
         return ResponseEntity.ok(enrichedLogService.searchByDto(enrichedLogEventDto, pageable));
     }
